337eb849366a216cc891c6976cc634665df323bd
freshprinceofcoders <milanosolanki27@gmail.com>
2020-12-23 13:27:25 +0000

    Finished it

--- Q5.py
+++ Q5.py
@@ -1,1 +1,14 @@
+#first_num // gcd(first_num, second_num) * second_num
+import math
+
+def compute_LCM():
+    ans = 1
+    for second_num in range(1,20):
+        ans *=  getLCM(ans, second_num)
+    return ans
+       
+def getLCM(ans, second_num):
+    return second_num //math.gcd(second_num, ans)
+
+print(compute_LCM())
 
